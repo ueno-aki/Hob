817c634760cfbcf708ca9b78a0258e694217144f
uenoaki <radaokuno2525@gmail.com>
2023-09-09 21:52:54 +0900

    update load motd

--- src/server.rs
+++ src/server.rs
@@ -1,14 +1,12 @@
 use crate::{
     components::{ClientId, Position},
-    player::Player,
+    player::Player, utils::get_option,
 };
-use anyhow::{anyhow, Context, Result};
+use anyhow::{anyhow, Result};
 use atomic_refcell::AtomicRefCell;
 use rust_raknet::RaknetListener;
 use sparsey::prelude::*;
-use std::fs;
 use std::sync::Arc;
-use yaml_rust::{Yaml, YamlLoader};
 
 pub struct Server {
     world: Arc<AtomicRefCell<World>>,
@@ -44,25 +42,17 @@ impl Server {
         Ok(())
     }
     fn load_motd() -> Result<String> {
-        let file = fs::read_to_string("./.server_properties")?;
-        let options = &YamlLoader::load_from_str(&file)?[0];
         let motd = format!(
             "MCPE;{};{};{};{};{};{};{};Survival;1;{}",
-            Self::get_option(options, "server_name")?,
-            Self::get_option(options, "protocol")?,
-            Self::get_option(options, "version")?,
+            get_option("server_name")?,
+            get_option("protocol")?,
+            get_option("version")?,
             100,
-            Self::get_option(options, "max_connection")?,
+            get_option("max_connection")?,
             rand::random::<u64>(),
-            Self::get_option(options, "description")?,
-            Self::get_option(options, "port")?
+            get_option("description")?,
+            get_option("port")?
         );
         Ok(motd)
-    }
-    fn get_option(yaml: &Yaml, name: &str) -> Result<String> {
-        Ok(yaml[name]
-            .as_str()
-            .context(format!("Failed to get option:{}", name))?
-            .to_owned())
     }
 }
