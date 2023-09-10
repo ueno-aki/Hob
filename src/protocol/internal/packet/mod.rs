6ce8d2dfa343e76c503da8f332a728d4f09ea449
uenoaki <radaokuno2525@gmail.com>
2023-09-10 14:17:01 +0900

    internal_pkt

---
+++ src/protocol/internal/packet/mod.rs
@@ -0,0 +1,36 @@
+#[derive(Debug)]
+pub enum InternalPacketKind {
+    CreateClient(CreateClient),
+    DestoryClient(DestoryClient)
+}
+
+#[derive(Debug)]
+pub struct CreateClient {
+    client_id:u64
+}
+
+#[derive(Debug)]
+pub struct DestoryClient {
+    client_id:u64
+}
+
+macro_rules! packet_impls {
+    ($($t:ident),*) => {
+        $(
+            impl From<$t> for InternalPacketKind {
+                fn from(value: $t) -> Self {
+                    InternalPacketKind::$t(value)
+                }
+            }
+            impl From<InternalPacketKind> for $t {
+                fn from(value: InternalPacketKind) -> Self {
+                    match value {
+                        InternalPacketKind::$t(kind) => kind,
+                        _ => panic!("Invalid PacketKind")
+                    }
+                }
+            }
+        )*
+    };
+}
+packet_impls!(CreateClient,DestoryClient);
